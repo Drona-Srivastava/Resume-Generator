9c096b5cdc09458b4f9e031249c2bf75eee2d742
Drona Srivastava <srivastavadrona@gmail.com>
2025-04-19 16:19:53 +0530

    done

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,9 @@ export default function Footer() {
     <div className="footer-container">
       <p className="footer-text">Created By</p>
       <div className="Names">
-        <p>Vyshnav M</p>
-        <p>Drona Srivastava</p>
-        <p>T Venkata Durga Sathwika</p>
+        <p>Vyshnav M 23BAI1195</p>
+        <p>Drona Srivastava 23BAI1314</p>
+        <p>T Venkata Durga Sathwika 23BAI1468</p>
       </div>
     </div>
   );
